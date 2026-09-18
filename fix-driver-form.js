ab0d6daea7d5022c3aa4a09e0b00266ec709c629
PrashantGuptacodecraft <adityagupta983869@gmail.com>
2026-09-19 01:18:41 +0530

    chore: remove all temporary patch and test scripts

--- fix-driver-form.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let file = fs.readFileSync('apps/web/app/(portal)/admin/drivers/page.tsx', 'utf8');
-
-file = file.replace(/function parseDate[\s\S]*?return input;\n\s*\}/, '');
-file = file.replace(/const handleDateInput[\s\S]*?\}\;\n/, '');
-file = file.replace(/inputMode="numeric"/g, 'inputMode="text"');
-
-fs.writeFileSync('apps/web/app/(portal)/admin/drivers/page.tsx', file);
--- fix-wizard-date.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let file = fs.readFileSync('apps/web/components/entry/entry-wizard.tsx', 'utf8');
-
-file = file.replace(/<input readOnly type="date"/g, '<input readOnly type="text"');
-
-fs.writeFileSync('apps/web/components/entry/entry-wizard.tsx', file);
